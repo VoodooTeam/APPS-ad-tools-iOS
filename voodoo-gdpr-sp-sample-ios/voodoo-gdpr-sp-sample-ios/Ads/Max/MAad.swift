606e7e26e962813b6a0255320521250248f223d2
gautier-gdx <gautier.gedoux@gmail.com>
2024-05-31 14:46:49 +0200

    Ad integration improvements

---
+++ voodoo-gdpr-sp-sample-ios/voodoo-gdpr-sp-sample-ios/Ads/Max/MAad.swift
@@ -0,0 +1,26 @@
+//
+//  MAad.swift
+//  Drop
+//
+//  Created by Gautier Gedoux on 31/05/2024.
+//
+
+import Foundation
+import AppLovinSDK
+
+class MAXAd: Ad {
+    let adUnit: String
+    let ad: MAAd
+    let type: AdType
+    var index: Int?
+    
+    var price: Double {
+        return ad.revenue
+    }
+    
+    init(adUnit: String, ad: MAAd, type: AdType) {
+        self.adUnit = adUnit
+        self.ad = ad
+        self.type = type
+    }
+}
